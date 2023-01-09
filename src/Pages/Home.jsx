952349f12f15bf0e431cdd6bd6c51c33f40d6011
eagrcode <elliot.robinson92@hotmail.com>
2023-01-09 18:56:06 +0000

    more Home cleanup

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import requests from "../requests";
-import { useQuery } from "@tanstack/react-query";
-
 import { Hero, MovieRows } from "../Components";
 
 function Home() {
   return (
     <>
       <Hero />
-      <MovieRows topRated={topRated} upcoming={upcoming} latest={latest} />
+      <MovieRows />
     </>
   );
 }
