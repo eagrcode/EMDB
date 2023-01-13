075a449e6c5e26874728d15ed8a0f30fc2115884
eagrcode <elliot.robinson92@hotmail.com>
2023-01-13 14:44:43 +0000

    add US lang to requests

--- src/requests.js
+++ src/requests.js
@@ -2,13 +2,13 @@ const API_KEY = "20be784f740b6b638c906dde5b35efae";
 
 const requests = {
   fetchTrending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`,
-  fetchUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=1`,
-  fetchLatest: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&primary_release_year=2022&page=1`,
-  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1`,
-  fetchPopularTV: `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&page=1`,
-  fetchTopRatedTV: `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&page=1`,
-  fetchMovies1: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=1`,
-  fetchMovies2: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=2`,
-  fetchMovies3: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=3`,
+  fetchUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=1&lang=us`,
+  fetchLatest: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&primary_release_year=2022&page=1&lang=us`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1&lang=us`,
+  fetchPopularTV: `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&page=1&lang=us`,
+  fetchTopRatedTV: `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&page=1&lang=us`,
+  fetchMovies1: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=1&lang=us`,
+  fetchMovies2: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=2&lang=us`,
+  fetchMovies3: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=3&lang=us`,
 };
 export default requests;
