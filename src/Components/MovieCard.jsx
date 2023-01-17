2df9fdbd0871abdea677000ac97f21674dcc4908
eagrcode <elliot.robinson92@hotmail.com>
2023-01-17 15:41:49 +0000

    add DetailsPage Link to MovieCard comp

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function MovieCard({ id, title, path, image }) {
   if (image) {
     return (
-      <div className="movie-card">
-        <img id="movie-card-img" src={`${path}${image}`} alt="movie-poster" />
-      </div>
+      <Link to={`/details/${id}`}>
+        <div className="movie-card">
+          <img id="movie-card-img" src={`${path}${image}`} alt="movie-poster" />
+        </div>
+      </Link>
     );
   }
 }
