34bc9c67d9037ff6b5b994f3b0ff7cc58395d004
eagrcode <elliot.robinson92@hotmail.com>
2023-01-21 17:49:40 +0000

    playing with responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import { FaInfoCircle, FaPlayCircle } from "react-icons/fa";
 import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence, LazyMotion } from "framer-motion";
 
 function Hero() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -72,48 +73,61 @@ function Hero() {
 
   // Success screen
   return (
-    <header id="hero-section">
-      <div
-        className="hero-container"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), hsl(240, 100%, 5%)), url("${path}${trending[currentSlide]?.backdrop_path}")`,
-        }}
-      >
-        <button className="hero-nav-btn">
-          <IoChevronBack className="icon" size={50} onClick={toPrevSlide} />
-        </button>
+    <AnimatePresence initial={false}>
+      <header id="hero-section">
+        <motion.div
+          key={trending[currentSlide].id}
+          initial={{ x: "-100%" }}
+          animate={{ x: 0 }}
+          exit={{ x: "-100%" }}
+          transition={{ duration: 0.4 }}
+          className="hero-container"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), hsl(240, 100%, 5%)), url("${path}${trending[currentSlide]?.backdrop_path}")`,
+          }}
+        >
+          <button className="hero-nav-btn">
+            <IoChevronBack className="icon" size={50} onClick={toPrevSlide} />
+          </button>
 
-        <div className="hero-overlay-container">
-          <div id="hero-card">
-            <img src={`${posterPath}${trending[currentSlide]?.poster_path}`} />
-          </div>
-          <div className="hero-text-container">
-            <div>
-              <h1>{trending[currentSlide]?.title}</h1>
+          <div className="hero-overlay-container">
+            <div id="hero-card">
+              <img
+                src={`${posterPath}${trending[currentSlide]?.poster_path}`}
+              />
             </div>
-            <div className="btn-container">
-              <button className="hero-btn">
-                Play Trailer <FaPlayCircle />
-              </button>
-              <Link
-                className="details-link"
-                to={`/details/${trending[currentSlide]?.id}`}
-              >
-                <button
-                  className="hero-btn"
-                  onClick={() => console.log(trending[currentSlide]?.id)}
-                >
-                  More Info <FaInfoCircle size={30} />
+            <div className="hero-text-container">
+              <div>
+                <h1>{trending[currentSlide]?.title}</h1>
+              </div>
+              <div className="btn-container">
+                <button className="hero-btn">
+                  Play Trailer <FaPlayCircle size={25} />
                 </button>
-              </Link>
+                <Link
+                  className="details-link"
+                  to={`/details/${trending[currentSlide]?.id}`}
+                >
+                  <button
+                    className="hero-btn"
+                    onClick={() => console.log(trending[currentSlide]?.id)}
+                  >
+                    More Info <FaInfoCircle size={25} />
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <button className="hero-nav-btn">
-          <IoChevronForward className="icon" size={50} onClick={toNextSlide} />
-        </button>
-      </div>
-    </header>
+          <button className="hero-nav-btn">
+            <IoChevronForward
+              className="icon"
+              size={50}
+              onClick={toNextSlide}
+            />
+          </button>
+        </motion.div>
+      </header>
+    </AnimatePresence>
   );
 }
 
